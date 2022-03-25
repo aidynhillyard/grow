7d8252ab663c02fc41283f5f4cf0544db1ea3ce1
Aidyn Hillyard <aidyn@example.com>
2022-03-25 15:24:21 -0500

    added react bootstrap header, minimal header css, minimal app css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,31 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 const Header = () => {
   return (
-    <div>
-        This is the header.
-    </div>
+    <Container className="header">
+      <Navbar bg="light" expand="lg">
+        <Container className="nav-bar">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link className="nav-bar-link" to="/">
+                Grow
+              </Link>
+              <Link className="nav-bar-link" to="/about">
+                About
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Navbar.Brand className="brand">
+            <p id="brand-text">Grow</p>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </Container>
   );
 };
 
