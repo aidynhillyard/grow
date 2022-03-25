826811b86b284dfe58a50c9cb84545f266b654e1
Aidyn Hillyard <aidyn@example.com>
2022-03-25 14:35:54 -0500

    updated structure of app.js and fixed footer.css file name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Grow from "./Components/Grow/Grow";
+import About from "./Components/About/About"
+import Container from "react-bootstrap/Container";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <p>Server is here.</p>
-    </div>
+    <Container id="page-container" className="App">
+      <Container id="content-wrap-app">
+        <Header />
+        <main>
+          <Routes>
+            <Route path='/' element={<Grow />}></Route>
+            <Route path='/about' element={<About />}></Route>
+          </Routes>
+        </main>
+      </Container>
+      <Container id="footer">
+        <Footer />
+      </Container>
+    </Container>
   );
 }
 
