0e521319a76100945def86417bff8807292196e1
Aidyn Hillyard <aidyn@example.com>
2022-03-25 17:24:24 -0500

    implemented minimal modal for care for plant button

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,17 @@
 import "./Home.css";
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Accordion from 'react-bootstrap/Accordion';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import { Link } from "react-router-dom";
 
 const Home = () => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
 return (
     <Container>
@@ -12,9 +19,23 @@ return (
   <Accordion.Item eventKey="0">
     <Accordion.Header>Examine Pots</Accordion.Header>
     <Accordion.Body>
-      <p>(Button: Care for Plant) Pot #1 -- Plant: (plantName), Age: (plantAge), Fully Grown: (fullyGrown)</p>
-      <p>(Button: Plant Seed) Pot #2 -- Empty</p>
-      <p>(Butotn: Plant Seed) Pot #3 -- Empty</p>
+      <p><Button variant="primary" onClick={handleShow}>
+      Care for Plant
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Care for Plant</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>You've spent one Action Point to care for your plant.</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Okay
+          </Button>
+        </Modal.Footer>
+      </Modal> Pot #1 -- Plant: (plantName), Age: (plantAge), Fully Grown: (fullyGrown)</p>
+      <p><Button variant="success">Plant a Seed</Button> Pot #2 -- Empty</p>
+      <p><Button variant="success">Plant a Seed</Button> Pot #3 -- Empty</p>
     </Accordion.Body>
   </Accordion.Item>
   <Accordion.Item eventKey="1">
