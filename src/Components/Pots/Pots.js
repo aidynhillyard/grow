be82abdc300844f02d7814694b0e6b87826ef897
Aidyn Hillyard <aidyn@example.com>
2022-03-28 20:14:28 -0500

    changed plant a seed button to a drop-down with plant options

--- src/Components/Pots/Pots.js
+++ src/Components/Pots/Pots.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
 import Planting from "../Planting/Planting";
 import { useEffect } from 'react'
 
@@ -20,8 +22,18 @@ const Pots = ({ checkIfPot1IsEmptyMarigold, actionPoints, setActionPoints, pot1I
     // useEffect???
 useEffect(() => console.log("Hello world"), [planting]);
 
+// Increment & Decrement functions
+const decrementActionPoints = () => {
+  setActionPoints(prevState => prevState - 1)
+}
+
+const incrementPot1Age = () => {
+  setPot1Age(prevState => prevState + 1)
+}
+
   return (
     <Container>
+      {/* Pot #1 */}
       <div><p>
       Pot #1 -- Plant: {pot1Type}, Age: {pot1Age}, Fully Grown: {pot1IsFullyGrown ? "true" : "false"}</p>
       </div>
@@ -29,11 +41,7 @@ useEffect(() => console.log("Hello world"), [planting]);
           Care for Plant
         </Button>
         </div>
-        <div style={{display: pot1IsEmpty === false ? "none" : "block"}}>
-        <Button variant="success" className="choose-a-seed" onClick={() => setPlanting(true)}>
-          Choose a Seed
-        </Button>
-        </div>
+        
       <div style={{display: planting === false ? "none" : "inline"}}></div>
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
@@ -49,16 +57,75 @@ useEffect(() => console.log("Hello world"), [planting]);
           </Modal.Footer>
         </Modal>
         
-      <p>
-        <Button variant="info" className="plant-a-seed">Plant a Seed</Button>
-        Pot #2 -- Empty
-      </p>
-      <p>
-        <Button variant="success" onClick={() => setPlanting(true)}>
-          Plant a Seed
+      <div style={{display: pot1IsEmpty === true ? "block" : "none"}}>
+      <DropdownButton id="dropdown-item-button" title="Choose a Seed" >
+  <Dropdown.Item as="button" onClick={() => {checkIfPot1IsEmptyMarigold(); setPlanting(false); decrementActionPoints(); setPot1Type("Marigold"); incrementPot1Age()}}>Marigold</Dropdown.Item>
+  <Dropdown.Item as="button" onClick={() => {checkIfPot1IsEmptyMarigold(); setPlanting(false); decrementActionPoints(); setPot1Type("Snapdragon"); incrementPot1Age()}}>Snapdragon</Dropdown.Item>
+  <Dropdown.Item as="button" onClick={() => {checkIfPot1IsEmptyMarigold(); setPlanting(false); decrementActionPoints(); setPot1Type("Sunflower"); incrementPot1Age()}}>Sunflower</Dropdown.Item>
+</DropdownButton>
+</div>
+{/* Pot #2 */}
+<div><p>
+      Pot #2 -- Plant: {pot1Type}, Age: {pot1Age}, Fully Grown: {pot1IsFullyGrown ? "true" : "false"}</p>
+      </div>
+        <div style={{display: pot1IsEmpty === true ? "none" : "block"}}><Button variant="primary" onClick={handleShow}>
+          Care for Plant
         </Button>
-        Pot #3 -- Empty
-      </p>
+        </div>
+        
+      <div style={{display: planting === false ? "none" : "inline"}}></div>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Care for Plant</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            You've spent one Action Point to care for your plant.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleClose}>
+              Okay
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        
+      <div style={{display: pot1IsEmpty === true ? "block" : "none"}}>
+      <DropdownButton id="dropdown-item-button" title="Choose a Seed" >
+  <Dropdown.Item as="button" onClick={() => {checkIfPot1IsEmptyMarigold(); setPlanting(false); decrementActionPoints(); setPot1Type("Marigold"); incrementPot1Age()}}>Marigold</Dropdown.Item>
+  <Dropdown.Item as="button" onClick={() => {checkIfPot1IsEmptyMarigold(); setPlanting(false); decrementActionPoints(); setPot1Type("Snapdragon"); incrementPot1Age()}}>Snapdragon</Dropdown.Item>
+  <Dropdown.Item as="button" onClick={() => {checkIfPot1IsEmptyMarigold(); setPlanting(false); decrementActionPoints(); setPot1Type("Sunflower"); incrementPot1Age()}}>Sunflower</Dropdown.Item>
+</DropdownButton>
+</div>
+{/* Pot #3 */}
+<div><p>
+      Pot #3 -- Plant: {pot1Type}, Age: {pot1Age}, Fully Grown: {pot1IsFullyGrown ? "true" : "false"}</p>
+      </div>
+        <div style={{display: pot1IsEmpty === true ? "none" : "block"}}><Button variant="primary" onClick={handleShow}>
+          Care for Plant
+        </Button>
+        </div>
+        
+      <div style={{display: planting === false ? "none" : "inline"}}></div>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Care for Plant</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            You've spent one Action Point to care for your plant.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleClose}>
+              Okay
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        
+      <div style={{display: pot1IsEmpty === true ? "block" : "none"}}>
+      <DropdownButton id="dropdown-item-button" title="Choose a Seed" >
+  <Dropdown.Item as="button" onClick={() => {checkIfPot1IsEmptyMarigold(); setPlanting(false); decrementActionPoints(); setPot1Type("Marigold"); incrementPot1Age()}}>Marigold</Dropdown.Item>
+  <Dropdown.Item as="button" onClick={() => {checkIfPot1IsEmptyMarigold(); setPlanting(false); decrementActionPoints(); setPot1Type("Snapdragon"); incrementPot1Age()}}>Snapdragon</Dropdown.Item>
+  <Dropdown.Item as="button" onClick={() => {checkIfPot1IsEmptyMarigold(); setPlanting(false); decrementActionPoints(); setPot1Type("Sunflower"); incrementPot1Age()}}>Sunflower</Dropdown.Item>
+</DropdownButton>
+</div>
 
       {/* Planting Div */}
       <div style={{display: planting === false ? "none" : "inline"}}>
