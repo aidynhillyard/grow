99f2acf959a8d187b83e3512c5550c10f0b58757
Aidyn Hillyard <aidyn@example.com>
2022-03-31 16:13:08 -0500

    completed ji3conversation feature

--- src/Components/Ji3Conversation/Ji3Conversation.js
+++ src/Components/Ji3Conversation/Ji3Conversation.js
@@ -1,61 +1,234 @@
 import "./Ji3Conversation.css";
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Alert from 'react-bootstrap/Alert'
+import { useState } from "react";
+import ji from "../../images/jiplaceholder.png";
+import player from "../../images/playeravplaceholder.png";
 
-const Ji3Conversation = ({ actionPoints, jiConversation }) => {
-
+const Ji3Conversation = ({
+  setJiTalk,
+  setShowFarhana,
+  setShowJi,
+  setShowAlba,
+}) => {
   // useStates
-  const [showJ1, setShowJ1] = useState(false);
-  const [showJ2, setShowJ2] = useState(false);
+  const [jiDialogue, setJiDialogue] = useState(1);
 
   return (
     <div>
-<Alert show={showJ1} variant="success">
-          <div className="npc-dialogue">
-            <p>
-              Step 3 of talking with Ji.  This is where you find out his favorite plant.
-            </p>
-          </div>
-          <hr />
-          <div className="player-dialogue">
-            <p>
-              Player dialogue goes here. Let's start with one dialogue option
-              first and not worry about player choices until later.
-            </p>
-          </div>
-          <div className="d-flex justify-content-end">
-            <Button
-              onClick={() => {
-                setShowJ2(true);
-                setShowJ1(false);
-              }}
-              variant="outline-success"
-            >
-              Next
-            </Button>
-          </div>
-        </Alert>
-        <Alert show={showJ2} variant="success">
-          <p>Ji's response. He says things depending on what you say.</p>
-          <div className="d-flex justify-content-end">
-            <Button onClick={() => setShowJ2(false)} variant="outline-success">
-              Close
-            </Button>
-          </div>
-        </Alert>
-
-        {!showJ1 && !showJ2 && (
-          <Button style={{ display: actionPoints > 0 ? "inline" : "none" }}
-            onClick={() => {
-              setShowJ1(true);
-              jiConversation();
-            }}
-          >
-            Talk
-          </Button>
-        )}
+      <div style={{ display: jiDialogue === 1 ? "inline" : "none" }}>
+        <div className="ji-dialogue-label fade-in-text">
+          <img src={ji} alt="Ji" id="ji-avatar" />
+          <p>Ji (he/him/his)</p>
         </div>
+        <p className="fade-in-text npc-dialogue">Oh hey, how’s it going?</p>
+        <button
+          className="fade-in-text"
+          aria-label="next"
+          onClick={() => {
+            setJiDialogue((prevState) => prevState + 1);
+          }}
+        >
+          &or;
+        </button>
+      </div>
+      <div style={{ display: jiDialogue === 2 ? "inline" : "none" }}>
+        <div className="player-dialogue-label fade-in-text">
+          <img src={player} alt="Plant avatar" id="player-avatar" />
+        </div>
+        <p className="fade-in-text player-dialogue">
+          Not too bad. What are you eating?
+        </p>
+        <button
+          className="fade-in-text"
+          aria-label="next"
+          onClick={() => {
+            setJiDialogue((prevState) => prevState + 1);
+          }}
+        >
+          &or;
+        </button>
+      </div>
+      <div style={{ display: jiDialogue === 3 ? "inline" : "none" }}>
+        <div className="ji-dialogue-label fade-in-text">
+          <img src={ji} alt="Ji" id="ji-avatar" />
+          <p>Ji (he/him/his)</p>
+        </div>
+        <p className="fade-in-text npc-dialogue">
+          <em>Gyeongdan</em>. They're like… rice balls with sesame seeds on the
+          outside, and they're filled with red bean paste. Want one?
+        </p>
+        <button
+          className="fade-in-text"
+          aria-label="next"
+          onClick={() => {
+            setJiDialogue((prevState) => prevState + 1);
+          }}
+        >
+          &or;
+        </button>
+      </div>
+      <div style={{ display: jiDialogue === 4 ? "inline" : "none" }}>
+        <div className="player-dialogue-label fade-in-text">
+          <img src={player} alt="Plant avatar" id="player-avatar" />
+        </div>
+        <p className="fade-in-text player-dialogue">
+          Sure, I’ll try it. ...Wow, this is delicious!
+        </p>
+        <button
+          className="fade-in-text"
+          aria-label="next"
+          onClick={() => {
+            setJiDialogue((prevState) => prevState + 1);
+          }}
+        >
+          &or;
+        </button>
+      </div>
+      <div style={{ display: jiDialogue === 5 ? "inline" : "none" }}>
+        <div className="ji-dialogue-label fade-in-text">
+          <img src={ji} alt="Ji" id="ji-avatar" />
+          <p>Ji (he/him/his)</p>
+        </div>
+        <p className="fade-in-text npc-dialogue">
+          Thanks, I made it myself. I started teaching myself how to cook after
+          my halmeoni, my grandma, passed away. Mom had been gone for awhile,
+          and Dad just kind of… checked out. So I figured someone should make
+          sure we were eating.
+        </p>
+        <button
+          className="fade-in-text"
+          aria-label="next"
+          onClick={() => {
+            setJiDialogue((prevState) => prevState + 1);
+          }}
+        >
+          &or;
+        </button>
+      </div>
+      <div style={{ display: jiDialogue === 6 ? "inline" : "none" }}>
+        <div className="player-dialogue-label fade-in-text">
+          <img src={player} alt="Plant avatar" id="player-avatar" />
+        </div>
+        <p className="fade-in-text player-dialogue">
+          That sounds like a lot for you to have to do. How old were you?
+        </p>
+        <button
+          className="fade-in-text"
+          aria-label="next"
+          onClick={() => {
+            setJiDialogue((prevState) => prevState + 1);
+          }}
+        >
+          &or;
+        </button>
+      </div>
+      <div style={{ display: jiDialogue === 7 ? "inline" : "none" }}>
+        <div className="ji-dialogue-label fade-in-text">
+          <img src={ji} alt="Ji" id="ji-avatar" />
+          <p>Ji (he/him/his)</p>
+        </div>
+        <p className="fade-in-text npc-dialogue">
+          It was only about four years ago. Wow, this memory stuff is so weird.
+          You sang at my grandma’s funeral, actually.
+        </p>
+        <button
+          className="fade-in-text"
+          aria-label="next"
+          onClick={() => {
+            setJiDialogue((prevState) => prevState + 1);
+          }}
+        >
+          &or;
+        </button>
+      </div>
+      <div style={{ display: jiDialogue === 8 ? "inline" : "none" }}>
+        <div className="player-dialogue-label fade-in-text">
+          <img src={player} alt="Plant avatar" id="player-avatar" />
+        </div>
+        <p className="fade-in-text player-dialogue">I… what?</p>
+        <button
+          className="fade-in-text"
+          aria-label="next"
+          onClick={() => {
+            setJiDialogue((prevState) => prevState + 1);
+          }}
+        >
+          &or;
+        </button>
+      </div>
+      <div style={{ display: jiDialogue === 9 ? "inline" : "none" }}>
+        <div className="ji-dialogue-label fade-in-text">
+          <img src={ji} alt="Ji" id="ji-avatar" />
+          <p>Ji (he/him/his)</p>
+        </div>
+        <p className="fade-in-text npc-dialogue">
+          Yeah. Well, at the memorial service afterward. The actual funeral was
+          a really formal family thing and all, but afterward, I had put
+          together a sort of “share memories” thing, and you sang this really
+          pretty song. You’ve got a great voice.
+        </p>
+        <button
+          className="fade-in-text"
+          aria-label="next"
+          onClick={() => {
+            setJiDialogue((prevState) => prevState + 1);
+          }}
+        >
+          &or;
+        </button>
+      </div>
+      <div style={{ display: jiDialogue === 10 ? "inline" : "none" }}>
+        <div className="player-dialogue-label fade-in-text">
+          <img src={player} alt="Plant avatar" id="player-avatar" />
+        </div>
+        <p className="fade-in-text player-dialogue">Um, thanks.</p>
+        <button
+          className="fade-in-text"
+          aria-label="next"
+          onClick={() => {
+            setJiDialogue((prevState) => prevState + 1);
+          }}
+        >
+          &or;
+        </button>
+      </div>
+      <div style={{ display: jiDialogue === 11 ? "inline" : "none" }}>
+        <div className="ji-dialogue-label fade-in-text">
+          <img src={ji} alt="Ji" id="ji-avatar" />
+          <p>Ji (he/him/his)</p>
+        </div>
+        <p className="fade-in-text npc-dialogue">
+          You brought her tulips. They were her favorite flower, and I guess
+          they’re mine now, too. …You should start singing again.
+        </p>
+        <button
+          className="fade-in-text"
+          aria-label="next"
+          onClick={() => {
+            setJiDialogue((prevState) => prevState + 1);
+          }}
+        >
+          &or;
+        </button>
+      </div>
+      <div style={{ display: jiDialogue === 12 ? "inline" : "none" }}>
+        <div className="player-dialogue-label fade-in-text">
+          <img src={player} alt="Plant avatar" id="player-avatar" />
+        </div>
+        <p className="fade-in-text player-dialogue">Maybe...</p>
+        <button
+          className="fade-in-text"
+          onClick={() => {
+            setJiDialogue((prevState) => prevState + 1);
+            setJiTalk(false);
+            setShowFarhana(true);
+            setShowJi(true);
+            setShowAlba(true);
+          }}
+        >
+          End
+        </button>
+      </div>
+    </div>
   );
 };
 
