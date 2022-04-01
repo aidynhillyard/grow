c81f70cc19396acbd984b7f51d8e72ca131d4bdc
Aidyn Hillyard <aidyn@example.com>
2022-04-01 16:25:17 -0500

    removed extraneous comma from footer component

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div>
-        GROW designed, and coded by Aidyn Hillyard, 2022
+        GROW designed and coded by Aidyn Hillyard, 2022
     </div>
   );
 };
