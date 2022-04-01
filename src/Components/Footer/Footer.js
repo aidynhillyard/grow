8cdf6b8b33022730eda14fc9b4be4df17ee928c9
Aidyn Hillyard <aidyn@example.com>
2022-04-01 18:25:57 -0500

    moved footer styling to footer.css

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,8 +2,8 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-        GROW designed and coded by Aidyn Hillyard, 2022
+    <div className="footer-div">
+        <p className="footer-text">Designed and coded by Aidyn Hillyard, 2022</p>
     </div>
   );
 };
