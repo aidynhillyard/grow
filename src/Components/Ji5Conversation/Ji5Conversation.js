3c4619003ae8b468cc163dbd26e68fc0fecb552b
Aidyn Hillyard <aidyn@example.com>
2022-03-31 19:08:25 -0500

    replaced placeholder dialogue in ji5conversation with actual dialogue

--- src/Components/Ji5Conversation/Ji5Conversation.js
+++ src/Components/Ji5Conversation/Ji5Conversation.js
@@ -27,43 +27,19 @@ const Ji5Conversation = ({
           setJiTalk(true);
           setJiDialogue((prevState) => prevState + 1);
         }}
-        style={{ display: actionPoints > 0 && !jiTalk && letter2IsRead ? "inline" : "none" }}
+        style={{
+          display:
+            actionPoints > 0 && !jiTalk && letter2IsRead ? "inline" : "none",
+        }}
       >
         Talk 5
       </button>
-      <div style={{ display: jiDialogue === 1 ? "inline" : "none" }}>
-        <div className="player-dialogue-label fade-in-text">
-          <img src={player} alt="Plant avatar" id="player-avatar" />
-        </div>
-        <p className="fade-in-text player-dialogue">
-          Five
-        </p>
-        <button
-          className="fade-in-text player-dialogue"
-          onClick={() => {
-            tradeDealJi();
-          }}
-        >
-          Give Plant
-        </button>
-        <button
-          className="fade-in-text"
-          aria-label="next"
-          onClick={() => {
-            setJiDialogue((prevState) => prevState + 1);
-          }}
-        >
-          &or;
-        </button>
-      </div>
       <div style={{ display: jiDialogue === 1 ? "inline" : "none" }}>
         <div className="ji-dialogue-label fade-in-text">
           <img src={ji} alt="Ji" id="ji-avatar" />
           <p>Ji (he/him/his)</p>
         </div>
-        <p className="fade-in-text npc-dialogue">
-          Is that…? It’s a tulip! Thank you!
-        </p>
+        <p className="fade-in-text npc-dialogue">Heya! How’ve you been?</p>
         <button
           className="fade-in-text"
           aria-label="next"
@@ -79,9 +55,7 @@ const Ji5Conversation = ({
           <img src={player} alt="Plant avatar" id="player-avatar" />
         </div>
         <p className="fade-in-text player-dialogue">
-          I was going through some of the photos in the house, and I found one
-          of you and me in your kitchen with your grandma, I think. And I just…
-          wanted to give this to you.
+          I’m doing okay. I read the letter from me, to me, that you gave me.
         </p>
         <button
           className="fade-in-text"
@@ -98,13 +72,7 @@ const Ji5Conversation = ({
           <img src={ji} alt="Ji" id="ji-avatar" />
           <p>Ji (he/him/his)</p>
         </div>
-        <p className="fade-in-text npc-dialogue">
-          You know… growing up in a Korean family, we’re not usually big on
-          therapy. Don’t want anyone to think anything's <em>wrong</em>. But,
-          after I lost my <em>halmeoni</em>, and things were pretty bad, I
-          finally decided to see someone. I drove two towns over just in case
-          word got back.
-        </p>
+        <p className="fade-in-text npc-dialogue">And…?</p>
         <button
           className="fade-in-text"
           aria-label="next"
@@ -119,7 +87,10 @@ const Ji5Conversation = ({
         <div className="player-dialogue-label fade-in-text">
           <img src={player} alt="Plant avatar" id="player-avatar" />
         </div>
-        <p className="fade-in-text player-dialogue">Did… it help?</p>
+        <p className="fade-in-text player-dialogue">
+          And I wrote it just after your grandma passed away. I… really loved
+          her.
+        </p>
         <button
           className="fade-in-text"
           aria-label="next"
@@ -136,10 +107,9 @@ const Ji5Conversation = ({
           <p>Ji (he/him/his)</p>
         </div>
         <p className="fade-in-text npc-dialogue">
-          Kinda. It’s not like it made everything better. But it helped me feel
-          like I could handle things better. …I saw footage of the earthquake on
-          TV, you know. It looked really intense. And I can’t imagine being
-          crushed under something like that.
+          I love her, too. Thanks again for the tulip, by the way. Did you know
+          that flowers have meanings? Tulips have a lot of different meanings,
+          but blue ones are supposed to mean trust.
         </p>
         <button
           className="fade-in-text"
@@ -155,7 +125,9 @@ const Ji5Conversation = ({
         <div className="player-dialogue-label fade-in-text">
           <img src={player} alt="Plant avatar" id="player-avatar" />
         </div>
-        <p className="fade-in-text player-dialogue">…</p>
+        <p className="fade-in-text player-dialogue">
+          Hm. That’s funny; the letter talked about trust, too.
+        </p>
         <button
           className="fade-in-text"
           aria-label="next"
@@ -172,8 +144,7 @@ const Ji5Conversation = ({
           <p>Ji (he/him/his)</p>
         </div>
         <p className="fade-in-text npc-dialogue">
-          I guess what I’m saying is that you’ve been through a lot, and, you
-          know, talking to someone is a thing. Oh, and this is for you.
+          It’s all connected. Life, and all that.
         </p>
         <button
           className="fade-in-text"
@@ -189,43 +160,8 @@ const Ji5Conversation = ({
         <div className="player-dialogue-label fade-in-text">
           <img src={player} alt="Plant avatar" id="player-avatar" />
         </div>
-        <p className="fade-in-text player-dialogue">A letter?</p>
-        <button
-          className="fade-in-text"
-          aria-label="next"
-          onClick={() => {
-            setJiDialogue((prevState) => prevState + 1);
-          }}
-        >
-          &or;
-        </button>
-      </div>
-      <div style={{ display: jiDialogue === 9 ? "inline" : "none" }}>
-        <div className="ji-dialogue-label fade-in-text">
-          <img src={ji} alt="Ji" id="ji-avatar" />
-          <p>Ji (he/him/his)</p>
-        </div>
-        <p className="fade-in-text npc-dialogue">
-          Yeah, you gave it to me a long time ago, and said to give it back to
-          you when you were older. Well, you’re older. We both are. So here it
-          is. And if you want, I can give you the name of my therapist.
-        </p>
-        <button
-          className="fade-in-text"
-          aria-label="next"
-          onClick={() => {
-            setJiDialogue((prevState) => prevState + 1);
-          }}
-        >
-          &or;
-        </button>
-      </div>
-      <div style={{ display: jiDialogue === 10 ? "inline" : "none" }}>
-        <div className="player-dialogue-label fade-in-text">
-          <img src={player} alt="Plant avatar" id="player-avatar" />
-        </div>
         <p className="fade-in-text player-dialogue">
-          I’ll think about it. Really. And thank you for the letter.
+          I’m beginning to feel like that's true.
         </p>
         <button
           className="fade-in-text"
