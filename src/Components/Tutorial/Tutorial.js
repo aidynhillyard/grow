dd0dad66db222df4d5083ab74ce9a4fcb68e6966
Aidyn Hillyard <aidyn@example.com>
2022-04-02 17:19:40 -0500

    removed dialogue images, completed css on tutorial

--- src/Components/Tutorial/Tutorial.js
+++ src/Components/Tutorial/Tutorial.js
@@ -2,6 +2,7 @@ import "./Tutorial.css";
 import { useState } from "react";
 import farhana from "../../images/farhanacircle.png";
 import player from "../../images/player.png";
+import note from "../../images/note.png";
 
 const Tutorial = ({ setLocation }) => {
   const [dialogue, setDialogue] = useState(0);
@@ -19,12 +20,12 @@ const Tutorial = ({ setLocation }) => {
       <div style={{ display: dialogue === 1 ? "inline" : "none" }}>
         <div className="farhana-dialogue-label fade-in-text">
           <img src={farhana} alt="Farhana" id="farhana-avatar" />
-          <p>Farhana</p>
+          <p>Farhana (she/her/hers)</p>
         </div>
         <p className="fade-in-text npc-dialogue">
-          Hey, Friend! Welcome back to town. When I heard you’d be moving back
-          into your old home, but in a wheelchair, I took the liberty
-          of installing some… modifications for you. What do you think?
+          Hey, Friend! Welcome back to town. When I heard you'd be moving back
+          into your old home, but using a wheelchair, I took the liberty of
+          installing some... modifications for you. What do you think?
         </p>
         <button
           className="fade-in-text"
@@ -33,7 +34,7 @@ const Tutorial = ({ setLocation }) => {
             setDialogue((prevState) => prevState + 1);
           }}
         >
-          &or;
+          &#9660;
         </button>
       </div>
       <div style={{ display: dialogue === 2 ? "inline" : "none" }}>
@@ -50,16 +51,16 @@ const Tutorial = ({ setLocation }) => {
             setDialogue((prevState) => prevState + 1);
           }}
         >
-          &or;
+          &#9660;
         </button>
       </div>
       <div style={{ display: dialogue === 3 ? "inline" : "none" }}>
         <div className="farhana-dialogue-label fade-in-text">
           <img src={farhana} alt="Farhana" id="farhana-avatar" />
-          <p>Farhana</p>
+          <p>Farhana (she/her/hers)</p>
         </div>
         <p className="fade-in-text npc-dialogue">
-          Absolutely. It’s a 4.8 degree grade. And I’ve been a carpenter for
+          Absolutely. It's a 4.8 degree grade. And I've been a carpenter for
           over a decade, remember?
         </p>
         <button
@@ -69,7 +70,7 @@ const Tutorial = ({ setLocation }) => {
             setDialogue((prevState) => prevState + 1);
           }}
         >
-          &or;
+          &#9660;
         </button>
       </div>
       <div style={{ display: dialogue === 4 ? "inline" : "none" }}>
@@ -84,18 +85,18 @@ const Tutorial = ({ setLocation }) => {
             setDialogue((prevState) => prevState + 1);
           }}
         >
-          &or;
+          &#9660;
         </button>
       </div>
       <div style={{ display: dialogue === 5 ? "inline" : "none" }}>
         <div className="farhana-dialogue-label fade-in-text">
           <img src={farhana} alt="Farhana" id="farhana-avatar" />
-          <p>Farhana</p>
+          <p>Farhana (she/her/hers)</p>
         </div>
         <p className="fade-in-text npc-dialogue">
-          Sorry. I didn’t mean anything by that; it’s just going to take awhile
+          Sorry. I didn't mean anything by that; it's just going to take awhile
           for me to adjust. I know you told me on the phone, but… you really
-          don’t remember anything?
+          don't remember anything?
         </p>
         <button
           className="fade-in-text"
@@ -104,7 +105,7 @@ const Tutorial = ({ setLocation }) => {
             setDialogue((prevState) => prevState + 1);
           }}
         >
-          &or;
+          &#9660;
         </button>
       </div>
       <div style={{ display: dialogue === 6 ? "inline" : "none" }}>
@@ -112,7 +113,7 @@ const Tutorial = ({ setLocation }) => {
           <img src={player} alt="Plant avatar" id="player-avatar" />
         </div>
         <p className="fade-in-text player-dialogue">
-          No, I don’t. The doctors say my memory will come back in time, but for
+          No, I don't. The doctors say my memory will come back in time, but for
           now, they said the best thing I can do is be in familiar surroundings
           and be patient. So, here I am.
         </p>
@@ -123,16 +124,16 @@ const Tutorial = ({ setLocation }) => {
             setDialogue((prevState) => prevState + 1);
           }}
         >
-          &or;
+          &#9660;
         </button>
       </div>
       <div style={{ display: dialogue === 7 ? "inline" : "none" }}>
         <div className="farhana-dialogue-label fade-in-text">
           <img src={farhana} alt="Farhana" id="farhana-avatar" />
-          <p>Farhana</p>
+          <p>Farhana (she/her/hers)</p>
         </div>
         <p className="fade-in-text npc-dialogue">
-          Well, we’re all glad to have you back. I’ll, um, leave you to it. I’m
+          Well, we're all glad to have you back. I'll, um, leave you to it. I'm
           sure you have a lot to unpack. Oh, and I added a few touches inside
           you might like, as well.
         </p>
@@ -143,7 +144,7 @@ const Tutorial = ({ setLocation }) => {
             setDialogue((prevState) => prevState + 1);
           }}
         >
-          &or;
+          &#9660;
         </button>
       </div>
       <div style={{ display: dialogue === 8 ? "inline" : "none" }}>
@@ -158,11 +159,11 @@ const Tutorial = ({ setLocation }) => {
             setDialogue((prevState) => prevState + 1);
           }}
         >
-          &or;
+          &#9660;
         </button>
       </div>
       <div style={{ display: dialogue === 9 ? "inline" : "none" }}>
-        <p className="fade-in-text">
+      <p className="fade-in-text meanwhile">
           <em>Inside the house...</em>
         </p>
         <button
@@ -172,10 +173,10 @@ const Tutorial = ({ setLocation }) => {
             setDialogue((prevState) => prevState + 1);
           }}
         >
-          &or;
+          &#9660;
         </button>
       </div>
-      <div
+      <div 
         style={{ display: dialogue > 9 && dialogue < 13 ? "inline" : "none" }}
       >
         <div className="player-dialogue-label fade-in-text">
@@ -183,8 +184,8 @@ const Tutorial = ({ setLocation }) => {
         </div>
       </div>
       <div style={{ display: dialogue === 10 ? "inline" : "none" }}>
-        <p className="fade-in-text">
-          Wow, Farhana really did think of everything. There’s handrails by the
+        <p className="fade-in-text player-dialogue">
+          Wow, Farhana really did think of everything. There's handrails by the
           toilet and the shower stall, and the shower stall even has a bench.
           Now to check out the kitchen....
         </p>
@@ -195,16 +196,16 @@ const Tutorial = ({ setLocation }) => {
             setDialogue((prevState) => prevState + 1);
           }}
         >
-          &or;
+          &#9660;
         </button>
       </div>
       <div style={{ display: dialogue === 11 ? "inline" : "none" }}>
-        <p className="fade-in-text">
-          Lowering the counters must’ve taken a lot of work–and money. I need to
+        <p className="fade-in-text player-dialogue">
+          Lowering the counters must've taken a lot of work–and money. I need to
           find a way to thank her.
         </p>
-        <p className="fade-in-text">
-          What are these empty pots for? Oh, there’s a note.
+        <p className="fade-in-text player-dialogue">
+          What are these empty pots for? Oh, there's a note.
         </p>
         <button
           className="fade-in-text"
@@ -213,22 +214,22 @@ const Tutorial = ({ setLocation }) => {
             setDialogue((prevState) => prevState + 1);
           }}
         >
-          &or;
+          &#9660;
         </button>
       </div>
       <div style={{ display: dialogue === 12 ? "inline" : "none" }}>
         <p className="fade-in-text">
           <em>
             I was going through my shed and found these piles of seed packets.
-            You always did like growing plants, so I thought I’d leave them here
-            for you, just in case. You know.
+            You always did like growing plants, so I thought I'd leave them here
+            for you, just in case.
           </em>
         </p>
         <p className="fade-in-text">
           <em>– Farhana</em>
         </p>
         <div>
-          <p className="fade-in-text">Image of plant will go here.</p>
+          <p className="fade-in-text"><img className="note-plant" src={note} alt="Line drawing of a leafy plant"/></p>
         </div>
         <button
           className="fade-in-text"
@@ -237,7 +238,7 @@ const Tutorial = ({ setLocation }) => {
             setDialogue((prevState) => prevState + 1);
           }}
         >
-          &or;
+          &#9660;
         </button>
       </div>
       {/* Tutorial */}
@@ -257,7 +258,7 @@ const Tutorial = ({ setLocation }) => {
               setDialogue((prevState) => prevState + 1);
             }}
           >
-            &or;
+            &#9660;
           </button>
         </div>
         <div style={{ display: dialogue === 14 ? "inline" : "none" }}>
@@ -274,7 +275,7 @@ const Tutorial = ({ setLocation }) => {
               setDialogue((prevState) => prevState + 1);
             }}
           >
-            &or;
+            &#9660;
           </button>
         </div>
         <div style={{ display: dialogue === 15 ? "inline" : "none" }}>
@@ -291,18 +292,19 @@ const Tutorial = ({ setLocation }) => {
               setDialogue((prevState) => prevState + 1);
             }}
           >
-            &or;
+            &#9660;
           </button>
         </div>
         <div style={{ display: dialogue === 16 ? "inline" : "none" }}>
           <p className="fade-in-text">
-            When your plant is fully grown, someone in town might be interested
-            in it, and you might be interested in what they will trade for it.
-            You can visit the town to talk to the townspeople.
+            When your plant is fully grown, you'll see it bloom. Someone in town
+            might be interested in it, and <em>you</em> might be interested in what they
+            will trade for it. You can visit the town to talk to the
+            townspeople.
           </p>
           <p className="fade-in-text">Happy planting!</p>
           <p className="go-home fade-in-text">
-            <button
+            <button className="end"
               onClick={() => {
                 setLocation("home");
               }}
